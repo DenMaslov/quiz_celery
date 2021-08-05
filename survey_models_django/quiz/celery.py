6322335fd6a1aae58e2eef54c9c9b3be36850165
DenMaslov <76794599+DenMaslov@users.noreply.github.com>
2021-08-05 12:41:51 +0300

    Update celery.py

--- survey_models_django/quiz/celery.py
+++ survey_models_django/quiz/celery.py
@@ -8,11 +8,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quiz.settings')
 app = Celery('quiz')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
-
-
-app.conf.beat_schedule = {
-    'task-every-day': {
-        'task': 'tests.tasks.update_csv_file',
-        'schedule': crontab(minute=0, hour=0),
-    }
-}
